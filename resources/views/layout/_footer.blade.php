fa395423565413efae628e462cb07e99997f9236
Leaves <1628043770@qq.com>
2019-07-21 01:01:05 +0000

    命名路由

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -2,6 +2,6 @@
   <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
   <a href="https://learnku.com/laravel/courses" target="_blank">刻意精进，每日练习</a>
   <div class="float-right">
-    <a href="/about">关于</a>
+    <a href="{{route('about')}}">关于</a>
   </div>
 </footer>
