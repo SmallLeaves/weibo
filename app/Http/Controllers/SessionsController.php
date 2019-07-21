3c769f166a4573d67d3f654cb2ba7710308c318b
Leaves <1628043770@qq.com>
2019-07-21 11:42:46 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -16,12 +16,16 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
         if(Auth::attempt($credentials)){
-            Auth::login($user);
             session()->flash('success','欢迎回来！');
             return redirect()->route('users.show',[Auth::user()]);
         }else{
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
+    }
+    public function destory(){
+        Auth::logout();
+        session()->flash('success','您已成功退出！');
+        return redirect('login');
     }
 }
