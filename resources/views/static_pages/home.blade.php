478f012c9c9511a2280809cde1614970c228c5a2
Leaves <1628043770@qq.com>
2019-07-21 00:19:13 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,13 @@
 @extends('layout.default')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+  <h1>Hello Laravel</h1>
+  <p class="lead">
+    你现在看到的是 <a href="https://fathomless-mountain-54618.herokuapp.com">小叶子的微博</a>主页
+  </p>
+  <p>一切，将从这里开始</p>
+  <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+  </p>
+</div>
 @stop
