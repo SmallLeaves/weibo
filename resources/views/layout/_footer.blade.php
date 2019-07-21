9386e3d5a4f41a0709f1393087f9029f98fa631e
Leaves <1628043770@qq.com>
2019-07-21 00:52:58 +0000

    切割头部和尾部子视图

---
+++ resources/views/layout/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target="_blank">刻意精进，每日练习</a>
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
