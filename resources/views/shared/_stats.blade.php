ce98868af15b4c29a944689eefd567d7d478c615
Leaves <1628043770@qq.com>
2019-07-22 06:31:16 +0000

    关注和粉丝列表页面

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,9 +1,9 @@
-<a href="#">
+<a href="{{route('users.followings',$user->id)}}">
   <strong class="stat" id="following">{{count($user->followings)}}</strong>关注
 </a>
-<a href="#">
+<a href="{{route('users.followers',$user->id)}}">
   <strong class="stat" id="followers">{{count($user->followers)}}</strong>粉丝
 </a>
-<a href="#">
+<a href="{{route('users.show',$user->id)}}">
   <strong class="stat" id="statuses">{{$user->statuses->count()}}</strong>微博
 </a>
