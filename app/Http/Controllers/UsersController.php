3e640b0ba28c20aa45319954eeecb4b319505767
Leaves <1628043770@qq.com>
2019-07-22 00:48:05 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -85,8 +85,8 @@ class UsersController extends Controller
         $name = 'Leaves';
         $to = $user->email;
         $subject = '感谢注册Weibo应用,请确认你的邮箱!';
-        Mail::send($view,$data,function($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
     /*激活功能*/
