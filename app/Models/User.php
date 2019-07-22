e7b7ca062deb7a1f9f7567c048bf716f221c9e9e
Leaves <1628043770@qq.com>
2019-07-22 05:53:49 +0000

    重新生成followers表+关注+取消关注+用户是否关注了某个用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,30 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at','desc');
     }
+    /*粉丝表*/
+    public function followers(){
+        return $this->belongsToMany(User::Class,'followers','user_id','followers_id');
+    }
+    /*用户关注的人*/
+    public function followings(){
+        return $this->belongsToMany(User::Class,'followers','followers_id','user_id');
+    }
+    /*关注*/
+    public function follow($user_ids){
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+    /*取消关注*/
+    public function unfollow($user_ids){
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    /*判断用户是否关注了某个人*/
+    public function isFollowing($user_id){
+        return $this->followings->contains($user_id);
+    }
 }
